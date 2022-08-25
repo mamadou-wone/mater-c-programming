ab07e98586155e0f8336782c9e17969c63e82e27
mamadou-wone <mamadu.wone@gmail.com>
2022-08-25 12:50:31 +0000

    learn strcat function

--- C-Souce/main.c
+++ C-Souce/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
+    char name[60];
+    printf("%s \n", name);
+    for (int i = 0; i < 7; i++)
+    {
+        strcat(name, "*");
+        printf("%s \n", name);
+    }
+    
     return 0;
 }
