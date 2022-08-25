356029135a89e8c8500cd595cf335fe5e714f526
mamadou-wone <mamadu.wone@gmail.com>
2022-08-25 19:13:22 +0000

    sum of digits

--- C-Souce/main.c
+++ C-Souce/main.c
@@ -5,8 +5,15 @@
 
 int main(int argc, char const *argv[])
 {
-    float decimal;
-    scanf("%f", &decimal);
-    printf("%f \n", (decimal - (int)decimal));
+    int number, num1, num2, num3;
+    scanf("%d", &number);
+    // scanf("%d", &d);
+    num3 = number % 10;
+    number = (int)(number / 10);
+    num2 = number % 10;
+    number = (int)(number / 10);
+    num1 = number % 10;
+    printf("[%d] [%d] [%d]\n" , num1, num2, num3);
+    printf("%d \n", num1+num2+num3);
     return 0;
 }
