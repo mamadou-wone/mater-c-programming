d75852de6dfd096bbd0fd2bc3ab9ea30abe85b89
mamadou-wone <mamadu.wone@gmail.com>
2022-08-26 13:05:09 +0000

    create getDigit function

--- C-Souce/main.c
+++ C-Souce/main.c
@@ -3,17 +3,35 @@
 #include <unistd.h>
 #include <string.h>
 
+void getDigits(int number)
+{
+    // int tab[8];
+    int digit;
+    int sum = 0;
+    while (number != 0)
+    {
+        digit = number % 10;
+        sum += digit;
+        printf("[%d] \n", digit);
+        number = (int) number / 10;
+    }
+    printf("Sum [%d]\n", sum);
+    // return tab;
+}
+
 int main(int argc, char const *argv[])
 {
-    int number, num1, num2, num3;
-    scanf("%d", &number);
-    // scanf("%d", &d);
-    num3 = number % 10;
-    number = (int)(number / 10);
-    num2 = number % 10;
-    number = (int)(number / 10);
-    num1 = number % 10;
-    printf("[%d] [%d] [%d]\n" , num1, num2, num3);
-    printf("%d \n", num1+num2+num3);
+    // int number, num1, num2, num3;
+    // scanf("%d", &number);
+    // // scanf("%d", &d);
+    // num3 = number % 10;
+    // number = (int)(number / 10);
+    // num2 = number % 10;
+    // number = (int)(number / 10);
+    // num1 = number % 10;
+    // printf("[%d] [%d] [%d]\n", num1, num2, num3);
+    // printf("%d \n", num1 + num2 + num3);
+
+    getDigits(54896347);
     return 0;
 }
