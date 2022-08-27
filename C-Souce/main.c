9dbe385daab023feceff57e93a301ef52e518abe
mamadou-wone <mamadu.wone@gmail.com>
2022-08-27 20:30:39 +0000

    pow function

--- C-Souce/main.c
+++ C-Souce/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 void getDigits(int number)
 {
@@ -21,17 +22,9 @@ void getDigits(int number)
 
 int main(int argc, char const *argv[])
 {
-    // int number, num1, num2, num3;
-    // scanf("%d", &number);
-    // // scanf("%d", &d);
-    // num3 = number % 10;
-    // number = (int)(number / 10);
-    // num2 = number % 10;
-    // number = (int)(number / 10);
-    // num1 = number % 10;
-    // printf("[%d] [%d] [%d]\n", num1, num2, num3);
-    // printf("%d \n", num1 + num2 + num3);
-
-    getDigits(54896347);
+    
+    // getDigits(54896347);
+    // printf("%d \n", (int)sqrt(9));
+    printf("[%d] \n", (int)pow(3, 2));
     return 0;
 }
